2cc572a63ea6bf25444eca4e306835e6154ae385
zxk20125 <781309842@qq.com>
2021-03-02 23:30:28 +0800

    更新

--- src/views/GRP/BillInfo.vue
+++ src/views/GRP/BillInfo.vue
@@ -3,10 +3,10 @@
         <div :wid="`1612854178100`" class="v2Container-1612854178100 V2Container" style="box-sizing:border-box;align-self:stretch;display:flex;flex-direction:row;align-items:flex-start;height:auto;" :if="true" ref="v2Container_1">
             <v2-form-input :wid="`1612854599723`" class="v2-form-input-1612854599723 V2Widget" :style="{'position':'relative','align-self':'auto','padding-left':0,'flex-shrink':0,'text-align':'left'}" :theme="{'size':''}" :disabled="false" :label="`运单号`" :placeholder="`请输入`" :title-mode="`col`" :label-witdh="`65px`" :input-type="`text`" :rows="2" :show-word-limit="false" :clearable="false" :is-required="false" :auto-complete="false" :if="true" ref="v2FormInput_4" :value.sync="waybill_id">
             </v2-form-input>
-            <v2-component-btn :wid="`1612855111983`" class="v2-component-btn-1612855111983 V2Widget" :style="{'align-self':'auto','margin-left':'10px'}" :theme="{'btnType':'primary','size':''}" :disabled="false" :loading="false" :btn-name="`查询`" :shape="`def`" :if="true" @_op_component-btn_btn:undefined="v2_component_btn_1613138459846" ref="v2ComponentBtn_1">
+            <v2-component-btn :wid="`1612855111983`" class="v2-component-btn-1612855111983 V2Widget" :style="{'align-self':'auto','margin-left':'10px','flex-shrink':0}" :theme="{'btnType':'primary','size':''}" :disabled="false" :loading="false" :btn-name="`查询`" :shape="`def`" :if="true" @_op_component-btn_btn:click="v2_component_btn_1613138459846" @_op_component-btn_btn:undefined="v2_component_btn_1614691893888" ref="v2ComponentBtn_1">
             </v2-component-btn>
         </div>
-        <v2-component-table :wid="`1612854187456`" class="v2-component-table-1612854187456 V2Widget" :style="{'align-self':'auto','margin-left':'0','width':'calc(100% - 20px)','flex-shrink':0,'height':'816.00px'}" :setPageCallback="setPageCallback" :toolBar="true" :tableData="billInfoList" :pageSize="pageSize" :totalCount="billTotalCount" :theme="{'tableType':'border','size':'small'}" :tooltip-effect="`dark`" :columns="[{'active':true,'type':'text','prop':'waybill_id','label':'运单号','handleLink':true,'handleLinkCB':'handleLinkCallback','linkTarget':'_blank','openTooltip':false,'handleTooltip':true,'tooltipContent':'提示','tooltipContentCallback':'tooltipContentCallback','useMinWidth':true,'headerAlign':'left','align':'left','sortable':false,'openFilter':false,'combi':[],'tagMap':[],'textMap':[],'iconMap':[],'filterMap':[]},{'active':true,'type':'text','prop':'status','label':'状态','handleLink':true,'handleLinkCB':'handleLinkCallback','linkTarget':'_blank','openTooltip':false,'handleTooltip':true,'tooltipContent':'提示','tooltipContentCallback':'tooltipContentCallback','useMinWidth':true,'headerAlign':'left','align':'left','sortable':false,'openFilter':false,'combi':[],'tagMap':[],'textMap':[],'iconMap':[],'filterMap':[]},{'active':true,'type':'text','prop':'pay','label':'是否付款','handleLink':true,'handleLinkCB':'handleLinkCallback','linkTarget':'_blank','openTooltip':false,'handleTooltip':true,'tooltipContent':'提示','tooltipContentCallback':'tooltipContentCallback','useMinWidth':true,'headerAlign':'left','align':'left','sortable':false,'openFilter':false,'combi':[],'tagMap':[],'textMap':[],'iconMap':[],'filterMap':[]},{'active':true,'type':'link','prop':'order_id','label':'订单号','handleLink':true,'handleLinkCB':'handleLinkCallback','linkTarget':'_blank','openTooltip':false,'handleTooltip':true,'tooltipContent':'提示','tooltipContentCallback':'tooltipContentCallback','useMinWidth':true,'headerAlign':'left','align':'left','sortable':false,'openFilter':false,'combi':[],'tagMap':[],'textMap':[],'iconMap':[],'filterMap':[]}]" :table-data="billInfoList" :selection-location="`left`" :t-height="`100%`" :t-max-height="`100%`" :open-index="false" :loading="false" :index-location="`left`" :open-page="true" :page-size="pageSize" :open-set-page-callback="true" :set-page-callback="`getbillInfo`" :total-count="billTotalCount" :tool-bar="true" :tool-title="`操作`" :tool-header-align="`left`" :tool-location="`right`" :tool-width="100" :tool-btns="[{'active':true,'btnName':'运单物流','type':'text','handler':'','rowName':' ','toolIcon':' ','rowValue':' '}]" :expand="false" :if="true" ref="v2ComponentTable_1">
+        <v2-component-table :wid="`1612854187456`" class="v2-component-table-1612854187456 V2Widget" :style="{'align-self':'auto','margin-left':'0','width':'calc(100% - 20px)','flex-shrink':0,'height':'890.00px'}" :setPageCallback="setPageCallback" :toolBar="true" :tableData="billInfoList" :pageSize="pageSize" :currentPage="currentPage" :totalCount="billTotalCount" :theme="{'tableType':'border','size':''}" :tooltip-effect="`dark`" :columns="[{'active':true,'type':'text','prop':'waybill_id','label':'运单号','handleLink':true,'handleLinkCB':'handleLinkCallback','linkTarget':'_blank','openTooltip':false,'handleTooltip':true,'tooltipContent':'提示','tooltipContentCallback':'tooltipContentCallback','useMinWidth':true,'headerAlign':'left','align':'left','sortable':false,'openFilter':false,'combi':[],'tagMap':[],'textMap':[],'iconMap':[],'filterMap':[]},{'active':true,'type':'text','prop':'status','label':'状态','handleLink':true,'handleLinkCB':'handleLinkCallback','linkTarget':'_blank','openTooltip':false,'handleTooltip':true,'tooltipContent':'提示','tooltipContentCallback':'tooltipContentCallback','useMinWidth':true,'headerAlign':'left','align':'left','sortable':false,'openFilter':false,'combi':[],'tagMap':[],'textMap':[],'iconMap':[],'filterMap':[]},{'active':true,'type':'text','prop':'pay','label':'是否付款','handleLink':true,'handleLinkCB':'handleLinkCallback','linkTarget':'_blank','openTooltip':false,'handleTooltip':true,'tooltipContent':'提示','tooltipContentCallback':'tooltipContentCallback','useMinWidth':true,'headerAlign':'left','align':'left','sortable':false,'openFilter':false,'combi':[],'tagMap':[],'textMap':[],'iconMap':[],'filterMap':[]},{'active':true,'type':'link','prop':'order_id','label':'订单号','handleLink':true,'handleLinkCB':'openEditBillPage','linkTarget':'_blank','openTooltip':false,'handleTooltip':true,'tooltipContent':'提示','tooltipContentCallback':'tooltipContentCallback','useMinWidth':true,'headerAlign':'left','align':'left','sortable':false,'openFilter':false,'combi':[],'tagMap':[],'textMap':[],'iconMap':[],'filterMap':[]}]" :table-data="billInfoList" :selection-location="`left`" :t-height="`100%`" :t-max-height="`100%`" :open-index="false" :loading="false" :index-location="`left`" :open-page="true" :page-size="pageSize" :open-set-page-callback="true" :set-page-callback="`getbillInfo`" :current-page="currentPage" :total-count="billTotalCount" :tool-bar="true" :tool-title="`操作`" :tool-header-align="`left`" :tool-location="`right`" :tool-width="100" :tool-btns="[{'active':true,'btnName':'运单物流','type':'text','handler':'','rowName':' ','toolIcon':' ','rowValue':' '}]" :expand="false" :if="true" ref="v2ComponentTable_1">
         </v2-component-table>
     </v2container>
 </template>
@@ -34,7 +34,7 @@
 
                         ]
                     },
-                    'mapping': {"1612854187456":[{"id":1612854187456,"modelValue":"setPageCallback","dataValue":"setPageCallback","type":""},{"id":1612854187456,"modelValue":"toolBar","dataValue":"true","type":""},{"id":1612854187456,"modelValue":"tableData","dataValue":"billInfoList","type":""},{"id":1612854187456,"modelValue":"pageSize","dataValue":"pageSize","type":""},{"id":1612854187456,"modelValue":"totalCount","dataValue":"billTotalCount","type":""}],"1612854599723":[{"id":1612854599723,"modelValue":"value","dataValue":"waybill_id","type":""}]}
+                    'mapping': {"1612854187456":[{"id":1612854187456,"modelValue":"setPageCallback","dataValue":"setPageCallback","type":""},{"id":1612854187456,"modelValue":"toolBar","dataValue":"true","type":""},{"id":1612854187456,"modelValue":"tableData","dataValue":"billInfoList","type":""},{"id":1612854187456,"modelValue":"pageSize","dataValue":"pageSize","type":""},{"id":1612854187456,"modelValue":"currentPage","dataValue":"currentPage","type":""},{"id":1612854187456,"modelValue":"totalCount","dataValue":"billTotalCount","type":""}],"1612854599723":[{"id":1612854599723,"modelValue":"value","dataValue":"waybill_id","type":""}]}
                 },
                 /* 运单号 */
                 'waybill_id': '',
@@ -53,6 +53,12 @@
             },
             /* 数据总条数 */
             'billTotalCount': {
+                'default': () => {
+                    return 1
+                }
+            },
+            /* 当前页码 */
+            'currentPage': {
                 'default': () => {
                     return 1
                 }
@@ -106,8 +112,6 @@
             },
             /**
              *  查询
-             *  @param $event $event
-             *  @param 组件的vue实例 vueIns
              */
             v2_component_btn_1613138459846($event, vueIns=this.$refs['v2ComponentBtn_1']) {
                 //*====AGREE-ACTION-START====*//
@@ -130,15 +134,57 @@
                 方法描述：获取客户地址信息
                 
                 **/
-                ctx.getStaffInfo();
+                ctx.getbillInfo(ctx.currentPage);
             },
             /**
              *  打开编辑子页面
+             *  @param 新参数 index
+             *  @param 新参数 row
              */
-            openEditStaffPage() {
-                // 函数内直接用 ctx 代替 this 访问 Vue 页面的数据。
-                /** @type {V2ViewType} */
+            openEditBillPage(index, row) {
+                //函数内直接用ctx代替this访问vue页面的数据。
                 const ctx = this;
+                
+                ctx.open({
+                    title: '编辑客户信息',
+                    path: '/edit/order',
+                    component: 'GRP/showBillDetail',
+                    type: 'SUB',
+                    closeOnClickModal: false,
+                    hideConfirmBtn: true,
+                    hideCancelBtn: true,
+                    width: "910px", //弹窗的宽
+                    height: "510px", //高，不设置默认是auto
+                    closeOnClickModal: true,//点击弹窗外部是否关闭弹窗，不设置默认是true：会关闭，false：不关闭
+                    params:{order_id:row.order_id},
+                    confirmCallback: (ret) => {
+                        ctx.$axios.post(`${axios.server}/orderInfo/edit`, ret).then(res => {
+                            //下一步的行为
+                            if (res.success) {
+                                ctx.$notify({
+                                    title: '客户信息编辑成功',
+                                    type: 'success'
+                                });
+                            } else {
+                                ctx.$notify({
+                                    title: res.msg,
+                                    type: 'error'
+                                });
+                
+                            }
+                        }).catch(function (error) {
+                            console.log(error);
+                            ctx.$notify({
+                                title: error.msg || '编辑失败',
+                                type: 'error'
+                            });
+                        });
+                
+                    },
+                    cancelCallback: (e) => {
+                        console.log('点击取消')
+                    }
+                })
             },
             /**
              *  判断是否付款
@@ -167,8 +213,40 @@
                 if(null==confirmTime) return"未揽件";
                 if(null==finishTime) return "运送中"
                 if(null!= finishTime) return"已完成";
+            },
+            /**
+             *  v2_component_btn_1614691890570
+             *  @param $event $event
+             *  @param 组件的vue实例 vueIns
+             */
+            v2_component_btn_1614691890570($event, vueIns) {
+                /**
+                * 更多其他参数可以参考element-ui官网:https://element.eleme.cn/#/zh-CN
+                **/
+                
+                //函数内直接用ctx代替this访问vue页面的数据。
+                const ctx = this;
+            },
+            /**
+             *  查询
+             *  @param $event $event
+             *  @param 组件的vue实例 vueIns
+             */
+            v2_component_btn_1614691893888($event, vueIns=this.$refs['v2ComponentBtn_1']) {
+                
+                //函数内直接用ctx代替this访问vue页面的数据。
+                const ctx = this;
+                //*++++AGREE-CODE-END++++*//
+                //*====AGREE-ACTION-END====*//
                 
                 
+                /**
+                方法名称：getbillInfo
+                方法描述：获取运单信息
+                参数1:page(新参数)
+                参数2:size(新参数)
+                **/
+                ctx.getbillInfo(ctx.currentPage);
             }
         },
         beforeCreate() {
--- src/views/GRP/showBillDetail.vue
+++ src/views/GRP/showBillDetail.vue
@@ -1,13 +1,18 @@
 <template>
-    <v2container :wid="`root`" style="width:100%;height:100%;display:flex;align-items:flex-start;align-self:stretch;box-sizing:border-box;flex-direction:column;flex-shrink:0;" class="V2Container">
-        <div :wid="`1614681220748`" class="v2Container-1614681220748 V2Container" style="box-sizing:border-box;align-self:stretch;display:flex;flex-direction:column;align-items:flex-start;" :if="true" ref="v2Container_1">
-            <div :wid="`1614681209975`" class="v2Container-1614681209975 V2Container" style="position:relative;overflow:hidden;display:block;height:226.00px;box-sizing:border-box;align-self:stretch;flex-direction:column;align-items:flex-start;min-width:50px;min-height:50px;animation-duration:1s;flex-shrink:0;" :if="true" ref="v2Container_3">
-                <div :wid="`1614681320182`" class="v2-component-text-1614681320182 V2Widget" style="box-sizing:border-box;align-self:auto;height:751.00px;width:1264.00px;" component="v2-component-text" ref="v2ComponentText_4">
-                    <p :style="{'word-break':'break-all'}">
-                        文本内容
-                    </p>
-                </div>
-            </div>
+    <v2container :wid="`root`" style="height:500.00px;width:900px;box-sizing:border-box;align-self:auto;border-right-width:0;border-left-width:0;border-top-width:0;border-bottom-width:0;margin-top:10px;margin-bottom:10px;flex-shrink:0;" class="V2Container">
+        <div :wid="`1614697445658`" class="v2Container-1614697445658 V2Container" style="box-sizing:border-box;align-self:stretch;display:flex;flex-direction:column;align-items:flex-start;height:418.00px;" :if="true" ref="v2Container_1">
+            <v2-component-text-value :wid="`1614697534034`" class="v2-component-text-value-1614697534034 V2Widget" :style="{'align-self':'auto','width':'322.00px'}" :title="`订单编号`" :overflow="`normal`" :overflow-line="3" :text-break="true" :type="`text`" :direction="`text-value-col`" :if="true" ref="v2ComponentTextValue_6" :value.sync="order_id">
+            </v2-component-text-value>
+            <v2-component-text-value :wid="`1614697544289`" class="v2-component-text-value-1614697534034 V2Widget" :style="{'align-self':'auto','width':'322.00px'}" :title="`下单用户`" :overflow="`normal`" :overflow-line="3" :text-break="true" :type="`text`" :direction="`text-value-col`" :if="true" ref="v2ComponentTextValue_7" :value.sync="order_id">
+            </v2-component-text-value>
+            <v2-component-text-value :wid="`1614697453934`" class="v2-component-text-value-1614697534034 V2Widget" :style="{'align-self':'auto','height':'24px','width':'322.00px'}" :title="`寄件地址`" :overflow="`normal`" :overflow-line="3" :text-break="true" :type="`text`" :direction="`text-value-col`" :if="true" ref="v2ComponentTextValue_8" :value.sync="order_id">
+            </v2-component-text-value>
+            <v2-component-text-value :wid="`1614697672395`" class="v2-component-text-value-1614697534034 V2Widget" :style="{'align-self':'auto','height':'24px','width':'322.00px'}" :title="`收件地址`" :overflow="`normal`" :overflow-line="3" :text-break="true" :type="`text`" :direction="`text-value-col`" :if="true" ref="v2ComponentTextValue_9" :value.sync="order_id">
+            </v2-component-text-value>
+            <v2-component-text-value :wid="`1614697706848`" class="v2-component-text-value-1614697534034 V2Widget" :style="{'align-self':'auto','height':'24.00px','width':'322.00px'}" :title="`物品信息`" :overflow="`normal`" :overflow-line="3" :text-break="true" :type="`text`" :direction="`text-value-col`" :if="true" ref="v2ComponentTextValue_10" :value.sync="order_id">
+            </v2-component-text-value>
+            <v2-component-text-value :wid="`1614697671782`" class="v2-component-text-value-1614697534034 V2Widget" :style="{'align-self':'auto','height':'24px','width':'322.00px'}" :title="`下单时间`" :overflow="`normal`" :overflow-line="3" :text-break="true" :type="`text`" :direction="`text-value-col`" :if="true" ref="v2ComponentTextValue_11" :value.sync="order_id">
+            </v2-component-text-value>
         </div>
     </v2container>
 </template>
@@ -35,8 +40,10 @@
 
                         ]
                     },
-                    'mapping': {}
+                    'mapping': {"1614693570282":[],"1614696808267":[],"1614696761906":[],"1614696892928":[{"id":1614696892928,"modelValue":"value","dataValue":"order_id","type":""}],"1614697123184":[{"id":1614697123184,"modelValue":"value","dataValue":"你好","type":""}],"1614697378341":[{"id":1614697378341,"modelValue":"value","dataValue":"order_id","type":""}],"1614697534034":[{"id":1614697534034,"modelValue":"value","dataValue":"order_id","type":""}],"1614697544289":[{"id":1614697544289,"modelValue":"value","dataValue":"order_id","type":""}],"1614697453934":[{"id":1614697453934,"modelValue":"value","dataValue":"order_id","type":""}],"1614697672395":[{"id":1614697672395,"modelValue":"value","dataValue":"order_id","type":""}]}
                 },
+                /* 订单编号 */
+                'order_id': '',
                 /* 版本号，请勿修改 */
                 '__VERSION': 4.0
             }
